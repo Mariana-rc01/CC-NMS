79712d7f432e4789afa9ffa774680dc5c1b80a1c
João Lobo <joao@lobo.is>
2024-11-17 21:39:59 +0000

    feat: send tasks after all agents are connected

--- src/server.py
+++ src/server.py
@@ -12,6 +12,8 @@ from server.agents_manager import AgentManager
 from server.task_json import load_tasks_json
 
 agent_manager = AgentManager()
+all_agents_registered = threading.Condition()
+required_agents = set()
 
 def server_packet_handler(message, client_address):
     if message.packet_type == PacketType.RegisterAgent:
@@ -21,22 +23,27 @@ def server_packet_handler(message, client_address):
 def handle_register_agent(message, client_address):
     agent_id = message.agent_id
     if agent_manager.register_agent(agent_id, client_address):
-        # Agent registered successfully.
         print(f"Agent {agent_id} registered.")
+
+        # Check if all required agents are registered
+        with all_agents_registered:
+            required_agents.discard(agent_id)
+            if not required_agents:  # All agents are registered
+                all_agents_registered.notify_all()
+
         return RegisterAgentPacketResponse(AgentRegistrationStatus.Success)
-    
-    # Agent already registered.
+
     return RegisterAgentPacketResponse(AgentRegistrationStatus.AlreadyRegistered)
 
 def distribute_tasks_to_agents(server, tasks):
     device_tasks = {}
 
-    # Group tasks by device.
+    # Group tasks by device
     for task in tasks:
         for device in task.devices:
             device_tasks.setdefault(device.device_id, []).append(task)
 
-    # Send tasks to each agent.
+    # Send tasks to each agent
     for device in device_tasks:
         agent_address = agent_manager.get_agent_by_id(device)
         if agent_address:
@@ -48,15 +55,23 @@ def main():
     print("Hello, from NMS Server!")
 
     tasks = load_tasks_json("tasks.json")
-    for task in tasks: print(task)
+    for task in tasks:
+        print(task)
+
+    # Store device IDs to check if all required agents are registered
+    for task in tasks:
+        for device in task.devices:
+            required_agents.add(device.device_id)
 
     server = UDPServer("0.0.0.0", 8080, server_packet_handler)
     alert_task_thread = threading.Thread(target=server.start, daemon=True)
     alert_task_thread.start()
 
-    # TODO: Send tasks when all agents needed are connected and registered.
-    time.sleep(10)
+    # Wait for all required agents to be registered
+    with all_agents_registered:
+        all_agents_registered.wait()
 
+    # Distribute tasks to agents
     distribute_tasks_to_agents(server, tasks)
 
     alert_task_thread.join()
